f8557c7d33ff20fbd692e3d0db35ea7d9756ca3d
tmo6ix <toyibomoses@gmail.com>
2022-10-12 10:05:42 +0100

    created checkout page along with checkout items and checkout cart fuunctionality

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navigation from './routes/navigation/navigation.component';
 import Home from "./routes/home/home.compponent";
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
-
+import Checkout from './routes/checkout/checkout.component';
 
 
 
@@ -22,6 +22,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
 
       </Route>
       
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { CartContext } from '../../contexts/cart.context'
 
@@ -10,13 +11,19 @@ import CartItem from '../cart-item/cart-item.component'
 
 
 const CartDropdown = () => {
+
+    const navigate = useNavigate()
+
+    const goToCheckoutHandler = () => {
+        navigate('/checkout')
+    }
     const {cartItems} = useContext(CartContext)
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
                 {cartItems.map(item => (<CartItem key={item.id} cartItem={item} />))}
             </div>
-            <Button>GO TO CHECKOUT</Button>
+            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
 
             
 
